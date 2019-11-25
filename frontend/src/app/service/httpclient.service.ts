66503e9d64c2c9db900a047e922d72dc159cc4de
Deepraj1996 <dprjpdnkr@gmail.com>
2019-11-25 18:28:15 +0530

    Added Email Registration

--- frontend/src/app/service/httpclient.service.ts
+++ frontend/src/app/service/httpclient.service.ts
@@ -1,19 +1,20 @@
+import { Zipcode } from './../zipcode.model';
 import { Registration } from './../registration/registration.model';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-export class Employee {
+export class Employee{
   constructor(
-    public name: string,
-    public email: string,
-    public contact: string,
-    public zipcode: number,
-    public city: string,
-    public state: string,
-    public country: string,
-    public agency: string,
-    public password: string
-  ) { }
+    public name:string,
+    public email:string,
+    public contact:string,
+    public zipcode:number,
+    public city:string,
+    public state:string,
+    public country:string,
+    public agency:string,
+    public password:string
+  ) {}
 }
 
 @Injectable({
@@ -26,27 +27,32 @@ export class HttpClientService {
       'Content-Type': 'application/json'
     })
   }
-  constructor(
-    private httpClient: HttpClient
-  ) {
+  constructor(private httpClient: HttpClient) {
     this.url = 'http://localhost:3000/api/register';
   }
-  public registerUser(user: Registration) {
-    return this.httpClient.post<Registration[]>(this.url, user, this.httpOptions);
+
+
+  getzipcode(value){
+    return this.httpClient.get<Zipcode>("http://localhost:8080/zipcode"+"/"+ value);
   }
+
+
 
   public createEmployee(employee) {
     console.log(employee);
     return this.httpClient.post<Employee>("http://localhost:8080/employees", employee);
   }
 
-
-  getHeaders() {
-    let username = 'admin'
-    let password = 'admin'
-
-    let basicString = 'Basic ' + window.btoa(username + ':' + password)
-    return basicString;
+ public registerUser(user: Registration) {
+    return this.httpClient.post<Registration[]>(this.url, user, this.httpOptions);
   }
+
+getHeaders(){
+  let username='admin'
+  let password='admin'
+
+  let  basicString='Basic '+window.btoa(username + ':' + password)
+  return basicString;
+}
 
 }
