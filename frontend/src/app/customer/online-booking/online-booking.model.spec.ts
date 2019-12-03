beae17704507b8a8d0636e4f22804c8a6436ac9a
Kiittyka <krithikaudupa@gmail.com>
2019-12-03 12:08:08 +0530

    Removed folder

---
+++ frontend/src/app/customer/online-booking/online-booking.model.spec.ts
@@ -0,0 +1,7 @@
+import { OnlineBooking } from './online-booking.model';
+
+describe('OnlineBooking', () => {
+  it('should create an instance', () => {
+    expect(new OnlineBooking()).toBeTruthy();
+  });
+});
