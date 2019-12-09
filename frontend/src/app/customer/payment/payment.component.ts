a242efe519e50ec31c2670c304bc0346fe0b3067
Deepraj1996 <dprjpdnkr@gmail.com>
2019-12-09 15:26:45 +0530

    Css and forms changed

--- frontend/src/app/customer/payment/payment.component.ts
+++ frontend/src/app/customer/payment/payment.component.ts
@@ -2,7 +2,7 @@ import { Product } from './../product-list/product.model';
 
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-payment',
@@ -13,21 +13,32 @@ import { FormBuilder } from '@angular/forms';
 
 export class PaymentComponent implements OnInit {
   ngOnInit() {
+    this.email = localStorage.getItem('email');
+    console.log(this.email)
+    this.payuform.email = this.email;
+
+    this.name = localStorage.getItem('username');
+    this.payuform.firstname = this.name;
+
+    this.phone = localStorage.getItem('contact');
+    this.payuform.phone = this.phone;
+
     this.productinfo = "product"
     this.amount = parseInt(sessionStorage.getItem('amount'))
     this.payuform.amount = (this.amount);
-    this.payuform.productinfo = this.productinfo
-    console.log(typeof(this.payuform.amount))
-    console.log(typeof(this.amount))
-    console.log(this.payuform.productinfo)
-    console.log(this.productinfo)
-
+    this.payuform.productinfo = this.productinfo;
+    console.log(this.amount)
+    console.log(this.payuform.amount)
   }
   public payuform: any = {};
   disablePaymentButton: boolean = true;
-  amount:Number;
-  constructor(private http: HttpClient) { }
-  productinfo : string
+  amount: Number;
+  email: string;
+  name: string;
+  phone: string;
+  constructor(private http: HttpClient, private fb: FormBuilder) { }
+  productinfo: string
+
 
   confirmPayment() {
     const paymentPayload = {
@@ -37,19 +48,20 @@ export class PaymentComponent implements OnInit {
       productinfo: this.payuform.productinfo,
       amount: this.payuform.amount
     }
+
     console.log(this.payuform.productinfo)
     console.log(paymentPayload)
     return this.http.post<any>('http://localhost:8970/payment/payment-details', paymentPayload).subscribe(
       data => {
-      console.log(data);
-      this.payuform.txnid = data.txnId;
-      this.payuform.surl = data.sUrl;
-      this.payuform.furl = data.fUrl;
-      this.payuform.key = data.key;
-      this.payuform.hash = data.hash;
-      this.payuform.txnid = data.txnId;
+        console.log(data);
+        this.payuform.txnid = data.txnId;
+        this.payuform.surl = data.sUrl;
+        this.payuform.furl = data.fUrl;
+        this.payuform.key = data.key;
+        this.payuform.hash = data.hash;
+        this.payuform.txnid = data.txnId;
         this.disablePaymentButton = false;
-    }, error1 => {
+      }, error1 => {
         console.log(error1);
       })
   }
